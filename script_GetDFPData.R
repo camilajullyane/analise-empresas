56407473a148e130c6a8dc155213ac438fda0a68
Victor Salgado <joaosalgado605@gmail.com>
2024-09-12 20:27:43 -0300

    primeiro filtro

--- script_GetDFPData.R
+++ script_GetDFPData.R
@@ -8,8 +8,39 @@ library(purrr)
 # Informações das Empresas
 
 df_info <- get_info_companies()
-names(df_info)
-print(df_info)
+
+# 1.1 Qtd Empresas por Setor e Segmentos de Governança Corporativas
+
+df_info %>%
+  count(SETOR_ATIV) %>%
+  arrange(desc(n))
+
+# 1.2 Filtrar setor com mais empresas canceladas: 
+
+qtd_setCancelada <- df_info %>%
+  filter(SIT_REG=="CANCELADA") %>%
+  count(SETOR_ATIV) %>%
+  arrange(desc(n)) 
+
+setMaisCanc <- as.vector(qtd_setCancelada[1, ])
+
+print(paste("Setor com mais empresas canceladas: ", setMaisCanc$SETOR_ATIV))
+print(paste("Número de empresas desse setor que foram canceladas: ", setMaisCanc$n))
+
+# 1.3 Filtrar do setor com mais empresas canceladas o maior motivo de cancelamento: 
+
+
+qtd_motCancel <- df_info %>%
+  filter(SETOR_ATIV==setMaisCanc$SETOR_ATIV) %>%
+  count(MOTIVO_CANCEL) %>%
+  arrange(desc(n))
+
+motivoMaisCanc <- as.vector(qtd_motCancel[1, ])
+
+print(paste("O motivo que mais cancelou empresas foi: ", motivoMaisCanc$MOTIVO_CANCEL))
+print(paste("Número de empresas desse setor que foram canceladas por esse motivo: ", motivoMaisCanc$n))
+
+# 1.4 Cria um df com os dados das empresas canceladas do setor
 
 qtd_empresasAtv <- df_info %>%
   filter(SIT_REG=="ATIVO") %>%
@@ -20,6 +51,3 @@ qtd_empresasAtv <- df_info %>%
 energia_eletrica <- df_info %>%
   filter(SIT_REG=="ATIVO" & SETOR_ATIV=="Energia Elétrica" & CATEG_REG=="Categoria A" & TP_MERC=="BOLSA")
 
-resultados <- energia_eletrica %>%
-  split(.$CD_CVM) %>% 
-  map(~ search_company(.)) 
