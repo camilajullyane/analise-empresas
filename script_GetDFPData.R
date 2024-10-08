268466bf480fe090074cd864d656e829bd8db5db
Camila Jullyane <134438133+camilajullyane@users.noreply.github.com>
2024-10-07 21:31:13 -0300

    Update script_GetDFPData.R

--- script_GetDFPData.R
+++ script_GetDFPData.R
@@ -12,11 +12,13 @@ Indice_liquidez <- function(ATCIRCULANTE,PSCIRCULANTE) {
   
 }
 
+
 Giro_estoque <- function(Custo_Venda, At_estoque){
   GIestoque = Custo_Venda*-1/At_estoque
   
   return(GIestoque)
 }
+
 
 Marg_Lucro <- function(Receita_financeiro, Receita_Venda){
   MARGlucro = Receita_financeiro/Receita_Venda
@@ -24,11 +26,57 @@ Marg_Lucro <- function(Receita_financeiro, Receita_Venda){
   return(MARGlucro)
 }
 
+
+Liquidez_Imediata <- function(At_caixa, Ps_circulante){
+  
+  LImediata = (At_caixa / Ps_circulante)
+  
+  return(LImediata)
+}
+
+
+Indice_Caixa <- function( At_caixa, Ps_circulante){
+  Icaixa = At_caixa / Ps_circulante
+  return(Icaixa) 
+}
+
+
+Giro_conta_receber <- function(Receita_Venda, At_receber) {
+  GIcontasReceber = Receita_Venda / At_receber
+  
+  return(GIcontasReceber)
+}
+
+
+ROA <- function(Receita_financeiro, At_total){
+  
+  RETSOBREATIVO = Receita_financeiro / At_total
+  
+  return(RETSOBREATIVO)
+}
+
+
+Endividamento_Total <- function(At_total, Balanco_comercial){
+  INDIENDIVTOTAL = (At_total - Balanco_comercial) / At_total
+
+  return(INDIENDIVTOTAL)
+}
+
+
+Multi_patrimonio_liq <- function(Balanco_comercial, At_total){
+  
+  MULTIPATRITOTAL = Balanco_comercial / At_total
+  
+  return(MULTIPATRITOTAL)
+}
+
+
 df_info <- get_info_companies()
 
-companies_ids <- c(4170, 21610, 7617, 2437)  # Lista de IDs das empresas
-first_year <- 2021
-last_year <- 2022
+# companies_ids <- c(4170, 7617, 2437)  # Lista de IDs das empresas
+companies_ids <- c(2437)  # Lista de IDs das empresas
+first_year <- 2022
+last_year <- 2023
 
 for (id_company in companies_ids) {
   company_name <- df_info %>%
@@ -48,21 +96,34 @@ for (id_company in companies_ids) {
   At_circulante <- unlist(fr_ativ[2, 4])
   At_estoque <- unlist(fr_ativ[16, 4])
   At_total <- unlist(fr_ativ[1, 4])
+  At_caixa <- unlist(fr_ativ[3, 4])
+  At_receber <- unlist(fr_ativ[10, 4])
   
   fr_passiv <- l_dfp$'DF Consolidado - Balanço Patrimonial Passivo' %>%
     select(DT_REFER, CD_CONTA, DS_CONTA, VL_CONTA)
   Ps_circulante <- unlist(fr_passiv[2, 4])
+  
+  Mut_Patrimoliq <- l_dfp$'DF Consolidado - Demonstração das Mutações do Patrimônio Líquido'%>%
+    select(DT_REFER, CD_CONTA, DS_CONTA, VL_CONTA)
   
   fr_dre <- l_dfp$'DF Consolidado - Demonstração do Resultado' %>%
     select(DT_REFER, CD_CONTA, DS_CONTA, VL_CONTA)
   Custo_Venda <- unlist(fr_dre[2, 4])
   Receita_financeiro <- unlist(fr_dre[53, 4])
   Receita_Venda <- unlist(fr_dre[1, 4])
+  Balanco_comercial <- unlist(Mut_Patrimoliq[152, 4])
   
   cat("Empresa ID:", id_company, "\n")
   cat("Nome da Empresa:", company_name, "\n")
-  cat("Indice de liquides: ", Indice_liquidez(At_circulante, Ps_circulante), "\n")
+  cat("Indice de liquidez: ", Indice_liquidez(At_circulante, Ps_circulante), "\n")
+  cat("Liquidez imediata: ", Liquidez_Imediata(At_circulante, Ps_circulante), "\n")
   cat("Giro de estoque: ", Giro_estoque(Custo_Venda, At_estoque), "\n")
-  cat("Margem de lucro: ", Marg_Lucro(Receita_financeiro, Receita_Venda))
+  cat("Giro de contas a receber: ", Giro_conta_receber(Receita_Venda, At_receber), "\n")
+  cat("Margem de lucro: ", Marg_Lucro(Receita_financeiro, Receita_Venda), "\n")
+  cat("Índice caixa: ", Indice_Caixa(At_caixa, Ps_circulante), "\n")
+  cat("ROA: ", ROA(Receita_financeiro, At_total), "\n")
+  Endividamento_Total
+  cat("Endividamento_Total: ", Endividamento_Total(At_total, Balanco_comercial), "\n")
+  cat("Multiplicador do Patrimônio Líquido: ", Multi_patrimonio_liq(Balanco_comercial, At_total), "\n")
+  
 }
-
