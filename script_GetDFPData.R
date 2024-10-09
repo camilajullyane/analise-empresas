4bc08e7b71ad3ed5958ca3c527ad1034b176d98c
Victor Salgado <joaosalgado605@gmail.com>
2024-10-09 19:27:53 -0300

    Update script_GetDFPData.R

--- script_GetDFPData.R
+++ script_GetDFPData.R
@@ -113,34 +113,59 @@ analisar_empresa <- function(companies_ids) {
     cat("Nome da Empresa:", company_name, "\n")
     cat("Indice de liquidez: ", Indice_liquidez(At_circulante, Ps_circulante), "\n")
     cat("Liquidez imediata: ", Liquidez_Imediata(At_circulante, Ps_circulante), "\n")
+    cut(Custo_Venda)
+    cut(At_estoque)
     cat("Giro de estoque: ", Giro_estoque(Custo_Venda, At_estoque), "\n")
     cat("Giro de contas a receber: ", Giro_conta_receber(Receita_Venda, At_receber), "\n")
     cat("Margem de lucro: ", Marg_Lucro(Receita_financeiro, Receita_Venda), "\n")
     cat("Índice caixa: ", Indice_Caixa(At_caixa, Ps_circulante), "\n")
     cat("ROA: ", ROA(Receita_financeiro, At_total), "\n")
     Endividamento_Total
-    cat("Endividamento_Total: ", Endividamento_Total(At_total, Balanco_comercial), "\n")
+    cat("Endividamento_Total: ", Endividamento_Total(At_total, Blanco_comercial), "\n")
     cat("Multiplicador do Patrimônio Líquido: ", Multi_patrimonio_liq(Balanco_comercial, At_total), "\n")
     
   }
 }
 
+#Análise das empresas (rode a função "analisar_empresas" para ver os indices)
+
 # Setor: Extração Mineral
 # Empresas: Vale
 # Empresa escolhida: Vale
-# Motivo:
+# Motivo: A Vale, empresa de extração de minério, além de não apresentar nenhum concorrente no 
+# ano de 2022 e 2023 ativo na bolsa de valores, sua produção de minério de ferro atingiu 
+# 321,2 milhões de toneladas, com alta de 4,3% sobre o ano anterior. O volume ficou acima 
+# da meta fixada para a empresa no ano passado, que era de 315 milhões de toneladas. 
+# Assim, mostrando-se uma empresa promissora e segura para investimentos.
+# Fonte: Relatório de produção e vendas da Vale no 4T23 e 2023
 analisar_empresa(c(4170))
 
 
 # Setor: Energia
-# Empresas: Eletrobras, Neoenergia, Serena Geração S.A.
-# Empresa escolhida:
-# Motivo:
+# Empresas: Eletrobras (escolha inicial), Neoenergia, Serena Geração S.A.
+# Empresa escolhida: Eletrobras
+# Motivo: Analisando as três empresas, percebemos que a Eletrobras mostrou uma vantagem econômica 
+# visto a comparação dos índices(índice de liquidez, liquidez imediata, índice caixa), se mostrando 
+# mais segura para investimentos. Além disso, seus resultados financeiros apresentados em seu 
+# relatório de 2023 mostraram um crescimento de 8% em relação ao ano anterior. Seu lucro 
+# líquido anual atingiu R$ 4,4 bilhões, um aumento de 21% em relação a 2022, demonstrando o 
+# impacto positivo do aumento das receitas de transmissão, a adequação de custos e despesas e 
+# a simplificação da estrutura administrativa da companhia.
+# Fonte: Relatório Anual 2023
+
 analisar_empresa(c(2437, 15539, 23426))
 
 
 # Setor: Banco
-# Empresas: Banco do Brasil, Banco BMG S/A, Bradesco S.A. 
-# Empresa escolhida:
-# Motivo:
+# Empresas: Banco do Brasil (escolha inicial), Banco BMG S/A, Bradesco S.A. 
+# Empresa escolhida: Banco do Brasil
+# Motivo: #Decidimos continuar com a empresa Banco do Brasil, pois, através dos resultados obtidos de 
+# comparação de indicadores (índice de liquidez, liquidez imediata, giro de estoque e índice 
+# caixa), é a que apresenta a maior vantagem. Além disso, foi-se analisado em seu relatório anual 
+# de 2023, um registro do maior lucro líquido ajustado da empresa, de mais de R$ 35,6 bilhões. 
+# Crescimento de 11,4% em relação a 2022. Além disso, foi adicionado R$ 86,1 bilhões em valor à 
+# sociedade, quando consideramos pagamento de impostos, salários, dividendos entre outros 
+# componentes, um crescimento de 7,3% em relação a 2022.
+# Fonte: Relatório anual 2023
 analisar_empresa(c(1023, 24600, 906))
+
